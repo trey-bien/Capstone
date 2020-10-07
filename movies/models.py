1a846a507b5a25fa4599704a977c00ecf32cffbc
Leanne Benson <stylistleannerowe@gmail.com>
2020-10-06 22:50:56 -0400

    fixed models, fully functioning actor and movie models, able to add images in the admin, all models are connected in admin panel, login, logout, auth, passing, ect.

--- movies/models.py
+++ movies/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-# from django.contrib.auth.models import User
-# from actors.models import Actor
+from django.contrib.auth.models import User
+from actors.models import Actor
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=50)
-    # director = models.ForeignKey(Actor, on_delete=models.CASCADE)
+    director = models.CharField(max_length=30, default=True)
     description = models.TextField()
     date_released = models.CharField(max_length=20)
     instructions = models.TextField()
-    photo = models.ImageField(upload_to ='uploads/', height_field=30, width_field=15)
-    # actors = models.ForeignKey(Actor, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='images/', null=True, blank=True)
+    actors = models.ForeignKey(Actor, on_delete=models.CASCADE, default=True)
     genre = models.TextField()
 
     def __str__(self):
